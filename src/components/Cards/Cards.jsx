fe534d7b94eab18c79dbafdf23764755f3f2068a
vheble <visheble@yahoo.com>
2020-05-27 00:12:20 +0530

    Added material components

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import styles  from './Cards.module.css';
 
-const Cards = () => {
+const Cards = (props) => {
+    console.log(props);
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2">Active Cases</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 } 
 
