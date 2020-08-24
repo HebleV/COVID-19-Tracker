d32c46c0a6d40a46bb16198c4590645de6bba4d9
HebleV <visheble@yahoo.com>
2020-08-24 23:04:40 +0530

    Added line chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import { fetchData, fetchDailyData } from '../../api';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+import styles from './Chart.module.css';
+
 
 const Chart = () => {
     const [dailyData, setDailyData] = useState({});
@@ -8,7 +11,18 @@ const Chart = () => {
             setDailyData(await fetchDailyData());
         }
         fetchAPI();
-    })
+    });
+
+    const lineChart = (
+        dailyData[0]
+        ? (
+            <Line 
+            data={{
+                labels: '',
+                datasets:[{}, {}]
+            }}
+        /> ) : null
+    );
     return (
         <h1>Chart</h1>
     )
